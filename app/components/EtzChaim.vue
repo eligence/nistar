ed73828c542123d5f6f4256d2320a8cafdd0b8f0
Eli Jayson <eli@vermill.io>
2025-08-14 15:18:46 -0400

    EtzChaim 3

--- app/components/EtzChaim.vue
+++ app/components/EtzChaim.vue
@@ -1,13 +1,5 @@
 <template>
-  <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://web.resource.org/cc/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="576" height="1010" viewBox="-2 2 288 505">
-    <metadata>
-      <rdf:RDF>
-        <cc:Work rdf:about="">
-          <dc:format>image/svg+xml</dc:format>
-          <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
-        </cc:Work>
-      </rdf:RDF>
-    </metadata>
+  <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="576" height="1010" viewBox="-2 2 288 505">
     <defs>
       <g id="Sephiroth">
         <circle cx="35.2041" cy="312.031" r="34.2186"/>
@@ -22,26 +14,29 @@
         <circle cx="142.454" cy="446.581" r="34.2186"/>
       </g>
     </defs>
-    <g style="opacity: 1; fill: none; fill-opacity: 1; fill-rule: nonzero; stroke:#ffffff;; stroke-width: 1.5; stroke-linecap: round; stroke-linejoin: bevel; stroke-miterlimit: 4; stroke-dasharray: none; stroke-dashoffset: 0pt; stroke-opacity: 1;">
+    <g style="opacity: 1; fill: none; fill-opacity: 1; fill-rule: nonzero; stroke:#ffffff; stroke-width: 1.5; stroke-linecap: round; stroke-linejoin: bevel; stroke-miterlimit: 4; stroke-dasharray: none; stroke-dashoffset: 0pt; stroke-opacity: 1;">
       <use xlink:href="#Sephiroth"/>
       <circle style="stroke:#ffffff;" cx="141.479" cy="163.831" r="34.2186"/>
     </g>
     <path :style="lineStyles" d="M 162.85,418.275 L 227.2,339.3" id="path3154"/>
-    <path :style="lineStyles" d="M 216.45,324.675 L 175.525,343.2" id="path3156" sodipodi:nodetypes="cc"/>
+    <path :style="lineStyles" d="M 216.45,324.675 L 175.525,343.2" id="path3156"/>
     <path :style="lineStyles" d="M 216.45,298.35 L 168.675,275.925" id="path3158"/>
-    <path :style="lineStyles" d="M 216.45,231.075 L 175.5,251.55" id="path3160" sodipodi:nodetypes="cc"/>
+    <path :style="lineStyles" d="M 216.45,231.075 L 175.5,251.55" id="path3160"/>
     <path :style="lineStyles" d="M 161.85,229.125 L 227.175,150.15" id="path3162"/>
     <path :style="lineStyles" d="M 69.275,124.8 L 214.525,123.825" id="path3166"/>
     <path :style="lineStyles" d="M 113.15,82.875 L 65.375,109.2" id="path3168"/>
     <path :style="lineStyles" d="M 57.525,151.125 L 122.85,228.1" id="path3172"/>
     <path :style="lineStyles" d="M 68.3,232.05 L 107.2,251.55" id="path3174"/>
     <path :style="lineStyles" d="M 112.125,274.95 L 64.35,294.45" id="path3176"/>
-    <path :style="lineStyles" d="M 141.375,291.5 L 141.375,317.85" id="path3178"/>
     <path :style="lineStyles" d="M 69.275,312 L 214.5,312" id="path3180"/>
     <path :style="lineStyles" d="M 68.3,322.725 L 111.15,340.275" id="path3182"/>
     <path :style="lineStyles" d="M 56.575,338.325 L 123.825,418.275" id="path3184"/>
 
+
+    <path :style="lineStyles" d="M 142.454,197.5 L 142.454,223.85" id="path3178-2"/>
+    <path :style="lineStyles" d="M 142.454,291.5 L 142.454,317.85" id="path3178"/>
     <path :style="lineStyles" d="M 142.454,100.4 L 142.454,130.3" id="path3170"/>
+    <path :style="lineStyles" d="M 142.454,386 L 142.454,412" id="path3284-7"/>
 
     <path :style="lineStyles" d="M 218.4,108.225 L 172.575,82.875" id="path3284-1"/>
     <path :style="lineStyles" d="M 35.120192,158.9 L 35.1,184.275" id="path3284-2"/>
@@ -49,7 +44,6 @@
     <path :style="lineStyles" d="M 248.63106,252.525 L 248.625,277.875" id="path3284-4"/>
     <path :style="lineStyles" d="M 248.625,157.975 L 248.625,184.275" id="path3284-5"/>
     <path :style="lineStyles" d="M 70.225,218.4 L 214.525,218.4" id="path3284-6"/>
-    <path :style="lineStyles" d="M 142.454,386 L 142.454,412" id="path3284-7"/>
   </svg>
 </template>
 <script setup lang="ts">
