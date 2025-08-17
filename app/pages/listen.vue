33c07e6171793d29b52f0990d1d0237d14fbae17
Eli Jayson <eli@vermill.io>
2025-08-17 13:35:23 -0400

    adds Go youtube video

--- app/pages/listen.vue
+++ app/pages/listen.vue
@@ -1,6 +1,7 @@
 <template>
   <PageLayout :header="'listen'">
-    <div class="flex justify-center align-center w-full">
+    <div class="flex flex-col justify-center items-center w-full gap-4 lg:gap-12">
+      <iframe class="w-100 lg:w-lg xl:w-xl aspect-video" src="https://www.youtube.com/embed/8vcHAquLtDY?si=HW9spTFvhjn7ltqS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       <iframe
         class="w-100 lg:w-lg xl:w-xl aspect-video"
         src="https://www.youtube.com/embed/videoseries?list=PLW7giUR5NtXgXVdPU8ObvsjMuDNaHkyvk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
