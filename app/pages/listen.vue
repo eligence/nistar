2e855b4fd853ee7523b60e720981aed369f81d8d
Eli Jayson <eli@vermill.io>
2025-08-18 21:41:25 -0400

    revert youtube embeds

--- app/pages/listen.vue
+++ app/pages/listen.vue
@@ -1,26 +1,11 @@
 <template>
   <PageLayout :header="'listen'">
-    <div class="flex flex-col justify-center items-center w-full gap-4 lg:gap-12 max-w-4xl mx-auto">
-      <!-- Single Video -->
-      <div class="w-full lg:w-4/5 xl:w-3/4">
-        <YouTubeEmbed
-          video-id="8vcHAquLtDY"
-          title="NISTAR Performance"
-          :params="'rel=0&modestbranding=1'"
-        />
-      </div>
-
-      <!-- Playlist -->
-      <div class="w-full lg:w-4/5 xl:w-3/4">
-        <YouTubeEmbed
-          video-id="PLW7giUR5NtXgXVdPU8ObvsjMuDNaHkyvk"
-          :is-playlist="true"
-          title="NISTAR Playlist"
-          :params="'rel=0&modestbranding=1'"
-        />
-      </div>
+    <div class="flex flex-col justify-center items-center w-full gap-4 lg:gap-12">
+      <iframe class="w-100 lg:w-lg xl:w-xl aspect-video" src="https://www.youtube.com/embed/8vcHAquLtDY?si=HW9spTFvhjn7ltqS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe
+        class="w-100 lg:w-lg xl:w-xl aspect-video"
+        src="https://www.youtube.com/embed/videoseries?list=PLW7giUR5NtXgXVdPU8ObvsjMuDNaHkyvk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-
     <template #details>
       <p>Ary Baldioceda - drums</p>
       <p>Eli Jayson - keyboards and saxophone</p>
@@ -28,3 +13,10 @@
     </template>
   </PageLayout>
 </template>
+
+<script setup lang="ts">
+
+</script>
+<style scoped>
+
+</style>
