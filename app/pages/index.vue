34498a337f7f07ce522cf05b5316878ada550278
Eli Jayson <eli@vermill.io>
2025-08-18 22:20:43 -0400

    home

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,21 +1,6 @@
 <template>
-  <PageLayout :header="'about'">
-    <p>Chassidic Funk, Soul, Jazz Originals</p>
-    <p>Imbuing music with Divine meditation</p>
-    <p>Opening mind and heart to trust and faith in The Creator with improvisation</p>
-    <p>Strengthening our connection to our essential being with wordless song</p>
-    <template #details>
-      <p>Ary Baldioceda - drums</p>
-      <p>Eli Jayson - keyboards and saxophone</p>
-    </template>
+  <PageLayout>
+   <h1>music is soul,</h1>
+   <h1>soul is life</h1>
   </PageLayout>
 </template>
-
-<script setup lang="ts">
-
-import PageLayout from "../components/PageLayout.vue";
-</script>
-
-<style scoped>
-
-</style>
