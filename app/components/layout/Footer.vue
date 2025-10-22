43f631d08c72c59d82ccc32bbb5b787c1623a5f5
Eli Jayson <eli@vermill.io>
2025-10-22 19:49:06 -0400

    footer container fix

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="backdrop-blur-sm border-t border-foreground/10 relative z-10" :class="{ 'hidden': isMobileMenuOpen }">
-    <div class="container mx-auto px-4 py-8">
+    <div class="mx-auto px-4 py-8">
       <div class="flex flex-col md:flex-row justify-between items-center">
         <!-- Copyright -->
         <div class="mb-4 md:mb-0">
